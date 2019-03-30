5d2a806fce5c23dccdbe435c3d93211b35133b3e
Saulo Santos <saulo.santos@conductor.com.br>
2019-03-30 18:14:33 -0300

    Criado estrutura do projeto com spring boot

---
+++ src/main/java/br/com/saulo/anp/AnpApplication.java
@@ -0,0 +1,13 @@
+package br.com.saulo.anp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnpApplication.class, args);
+	}
+
+}
